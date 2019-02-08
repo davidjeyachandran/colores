542b45d2a4d928475178db9193c49e7fcca82830
David Jeyachandran <davidjeyachandran@gmail.com>
2019-02-08 00:35:30 -0500

    Add Submit handler

--- server.js
+++ server.js
@@ -15,25 +15,29 @@ MongoClient.connect('mongodb://admin:password10@ds123625.mlab.com:23625/dj-quote
     });
 })
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
     db.collection('quotes').find().toArray(function(err, results) {
-        console.log(results);      
+        // console.log(results);      
     })
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-app.post('/quotes', (req, res) => {
+app.post('/submit', (req, res) => {
     console.log(req.body);
     db.collection('quotes').insertOne(req.body, (err, result) => {
         if (err) return console.log(err)
 
         console.log('Saved to database');
-        res.redirect('/')
-        
+        res.redirect('/submit')
     })
-   
 })
 
-app.get('/quotes', function(req, res) {
-    res.send('Quotes post');
+app.get('/submit', function(req, res) {
+    db.collection('quotes').find().toArray(function(err, results) {
+        console.log(results)
+        res.send('<h1>Hey</h1>');
+        // send HTML file populated with quotes here
+      })
 });
