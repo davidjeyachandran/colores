6315acd3f4c743444800a2127c06e2d1a9bf15fe
David Jeyachandran <davidjeyachandran@gmail.com>
2019-02-08 19:36:20 -0500

    Remove username password from file

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const app = express();
+const dbLogin = require('./dbLogin');
 var db;
 
-app.use(bodyParser.urlencoded({extended: true}))
+console.log(dbLogin);
 
-MongoClient.connect('mongodb://admin:password10@ds123625.mlab.com:23625/dj-quotes', { useNewUrlParser: true }, (err, client) => {
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+MongoClient.connect('mongodb://' + dbLogin.username + ':' + dbLogin.password + '@ds123625.mlab.com:23625/dj-quotes', { useNewUrlParser: true }, (err, client) => {
     if (err) return console.log(err);
     
     db = client.db('dj-quotes')
@@ -30,14 +34,28 @@ app.post('/submit', (req, res) => {
         if (err) return console.log(err)
 
         console.log('Saved to database');
-        res.redirect('/submit')
+        res.redirect('/results')
     })
 })
 
-app.get('/submit', function(req, res) {
+app.get('/results', function(req, res) {
     db.collection('quotes').find().toArray(function(err, results) {
         console.log(results)
-        res.send('<h1>Hey</h1>');
+        // res.send('<h1>Hey</h1>');
+        var colorList = getColorList(results);
+        console.log(colorList);
+        res.render('results.ejs', {colorList: colorList})
         // send HTML file populated with quotes here
       })
 });
+
+function getColorList(results) {
+    var colorList = new Object();
+    
+    results.forEach(function(item){
+        if (isNaN(colorList[item.color])) colorList[item.color]=0;
+        colorList[item.color]++;
+    });
+
+    return colorList;
+}
